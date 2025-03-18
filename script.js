11ab13d8a8425a54c3eddc68826bfd18a9ada551
Laurentino Jaimes <ljaimes92@gmail.com>
2025-03-18 00:37:05 -0400

    Add initial project files including HTML, CSS, JS, and got the site styled a bit

---
+++ script.js
@@ -0,0 +1,17 @@
+const myLibrary = [
+    {
+        title: "",
+        aurthor: "",
+        pages: 0,
+        read: false,
+    },
+    
+];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
